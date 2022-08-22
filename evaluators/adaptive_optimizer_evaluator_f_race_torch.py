6e81e061df1c4a26b6a711956f84defdf8f73dce
soren5 <pedrofilipecarvalho@mac.com>
2022-08-22 18:14:05 +0100

    Pytorch initial implementation

---
+++ evaluators/adaptive_optimizer_evaluator_f_race_torch.py
@@ -0,0 +1,158 @@
+import csv
+from utils.data_functions import load_fashion_mnist_training
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from optimizers.custom_optimizer import CustomOptimizerTorch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import sys
+
+import numpy as np
+import datetime
+experiment_time = datetime.datetime.now()
+
+def train_model_torch(phen_params):
+    phen, params = phen_params
+    print(params['EPOCHS'])
+    validation_size = params['VALIDATION_SIZE']
+    test_size = params['TEST_SIZE'] 
+    batch_size = params['BATCH_SIZE']
+    epochs = params['EPOCHS']
+    patience = params['PATIENCE']
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    batch_size = 4
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    class Net(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.fc1 = nn.Linear(16 * 5 * 5, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 10)
+
+        def forward(self, x):
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = torch.flatten(x, 1) # flatten all dimensions except batch
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
+
+
+    net = Net()
+
+
+    criterion = nn.CrossEntropyLoss()
+
+    optimizer = CustomOptimizerTorch(net.parameters(), phen=phen)
+
+    for epoch in range(epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+    PATH = './cifar_net.pth'
+    torch.save(net.state_dict(), PATH)
+
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+
+    # print images
+    print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+    net = Net()
+    net.load_state_dict(torch.load(PATH))
+
+    net = Net()
+    net.load_state_dict(torch.load(PATH))
+
+    _, predicted = torch.max(outputs, 1)
+
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                                for j in range(4)))
+
+    correct = 0
+    total = 0
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            # calculate outputs by running images through the network 
+            outputs = net(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
+
+    # prepare to count predictions for each class
+    correct_pred = {classname: 0 for classname in classes}
+    total_pred = {classname: 0 for classname in classes}
+
+    # again no gradients needed
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data    
+            outputs = net(images)    
+            _, predictions = torch.max(outputs, 1)
+            # collect the correct predictions for each class
+            for label, prediction in zip(labels, predictions):
+                if label == prediction:
+                    correct_pred[classes[label]] += 1
+                total_pred[classes[label]] += 1
+
+    total_accuracy = 0
+    
+    # print accuracy for each class
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        total_accuracy += accuracy
+        print("Accuracy for class {:5s} is: {:.1f} %".format(classname, 
+                                                    accuracy))
+    total_accuracy /= len(classes)
+    print(f"Total Accuracy {total_accuracy}")
+    return total_accuracy / 100, 0
--- test/test_engine.py
+++ test/test_engine.py
@@ -127,5 +127,35 @@ def test_short_run():
     evaluation_function = LROptimizer()
     sge.evolutionary_algorithm(parameters=params, evaluation_function=evaluation_function)
 
+def test_pytorch():
+    import sge
+    from main import Optimizer_Evaluator_Torch
+    evaluation_function = Optimizer_Evaluator_Torch()
+    params = {
+        "POPSIZE": 2,
+        "GENERATIONS": 2,
+        "ELITISM": 0,   
+        "SEED": 0,                
+        "PROB_CROSSOVER": 0.0,
+        "PROB_MUTATION": 0.15,
+        "TSIZE": 2,
+        "GRAMMAR": 'grammars/adaptive_autolr_grammar_torch.txt',
+        "EXPERIMENT_NAME": 'dumps/test_torch',
+        "RUN": 1,
+        "INCLUDE_GENOTYPE": True,
+        "SAVE_STEP": 1,
+        "VERBOSE": True,
+        "EPOCHS": 2,
+        "VALIDATION_SIZE": 10,
+        "TEST_SIZE": 59980,
+        "BATCH_SIZE": 5,
+        "MIN_TREE_DEPTH": 6,
+        "MAX_TREE_DEPTH": 17,
+        "FITNESS_FLOOR": 0,
+        "PREPOPULATE": False,
+        "PATIENCE": 0,
+    }
+    sge.evolutionary_algorithm(evaluation_function=evaluation_function, parameters=params)
+
 if __name__ == "__main__":
-    test_default_parameters()
+    test_pytorch()
