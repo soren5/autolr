6e84edeacd8029c9ea316857cbcc417d7a9084a0
soren5 <pedrofilipecarvalho@mac.com>
2022-12-02 14:55:23 +0000

    Add clean up utilities

---
+++ utils/utilities.py
@@ -0,0 +1,12 @@
+import os
+
+def delete_directory(experiment_name, run):
+    path = os.path.join(experiment_name, run)
+    for file_name in os.listdir(path):
+        # construct full file path
+        file = os.path.join(path, file_name)
+        if os.path.isfile(file):
+            print('Deleting file:', file)
+            os.remove(file)
+    os.rmdir(path)
+    os.rmdir(experiment_name)
