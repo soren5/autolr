2d43a548f13ca621e7a284703aac6c0e52fa9168
soren5 <pedrofilipecarvalho@mac.com>
2022-12-04 23:36:56 +0000

    Reorganize tests

--- test/test_engine.py
+++ test/test_engine.py
@@ -48,10 +48,10 @@ def test_engine():
 def test_default_parameters():
     import sge.grammar as grammar
     import sge    
-    from main import Optimizer_Evaluator_Tensorflow
+    from main import Optimizer_Evaluator
     from utils import create_models
     create_models.create_models()
-    evaluation_function = Optimizer_Evaluator_Tensorflow()
+    evaluation_function = Optimizer_Evaluator()
 
     sge.evolutionary_algorithm(evaluation_function=evaluation_function)
 
@@ -60,11 +60,11 @@ def test_mutation_errors():
     import sge.grammar as grammar
     import sge
     import yaml
-    from main import Optimizer_Evaluator_Tensorflow
+    from main import Optimizer_Evaluator
     from utils import create_models
 
     create_models.create_models()
-    evaluation_function = Optimizer_Evaluator_Tensorflow()
+    evaluation_function = Optimizer_Evaluator()
 
     with open("parameters/adaptive_autolr.yml", 'r') as ymlfile:
         parameters = yaml.load(ymlfile, Loader=yaml.FullLoader)
@@ -75,8 +75,8 @@ def test_mutation_errors():
     except AssertionError:
         print("Caught Invalid Size Error successfully")
     else:
-        raise AssertionError
-    parameters['PROB_MUTATION'] = [0.2, 0.2]
+        raise AssertionError("Failed to catch Invalid Size Error successfully")
+    parameters['PROB_MUTATION'] = [1.0, 1.0]
     try:
         sge.evolutionary_algorithm(parameters=parameters, evaluation_function=evaluation_function)
     except Exception:
@@ -84,40 +84,6 @@ def test_mutation_errors():
     else:
         raise AssertionError("Failed to catch invalid Mutation Type Error successfully")
     ut.delete_directory(parameters['EXPERIMENT_NAME'], "run_1")
-        
-def test_short_run():
-    import sge.grammar as grammar
-    import sge
-    from main import Optimizer_Evaluator_Tensorflow
-    from utils import create_models
-    create_models.create_models()
-    parameters = {
-        "POPSIZE": 2,
-        "GENERATIONS": 2,
-        "ELITISM": 0,   
-        "PROB_CROSSOVER": 0.0,
-        "PROB_MUTATION": 0.1,
-        "TSIZE": 2,
-        "GRAMMAR": 'grammars/adaptive_autolr_grammar_mutate_level.txt',
-        "EXPERIMENT_NAME": 'test_parameter',
-        "RUN": 1,
-        "INCLUDE_GENOTYPE": True,
-        "SAVE_STEP": 1,
-        "VERBOSE": True,
-        "EPOCHS": 2,
-        "MODEL": 'models/mnist_model.h5',
-        "VALIDATION_SIZE": 10,
-        "FITNESS_SIZE": 59590,
-        "BATCH_SIZE": 5,
-        "MIN_TREE_DEPTH": 6,
-        "MAX_TREE_DEPTH": 17,
-        "FITNESS_FLOOR": 0,
-        "PREPOPULATE": False,
-        "FAKE_FITNESS": True,
-    }
-    evaluation_function = Optimizer_Evaluator_Tensorflow()
-    sge.evolutionary_algorithm(parameters=parameters, evaluation_function=evaluation_function)
-
 
 
 def test_parameters():
@@ -218,110 +184,3 @@ def test_reevaluation():
     indiv_2 = copy.deepcopy(indiv)
     evaluate(indiv_2, FitnessEvaluator())
     assert indiv == indiv_2
-def test_short_run_cifar10():
-    import sge.grammar as grammar
-    import sge
-    from main import Optimizer_Evaluator_Tensorflow
-    from utils import create_models
-    create_models.create_models()
-    parameters = {
-        "POPSIZE": 2,
-        "GENERATIONS": 2,
-        "ELITISM": 0,   
-        "SEED": 0,                
-        "PROB_CROSSOVER": 0.0,
-        "PROB_MUTATION": {
-        0: 0.0, 
-        1: 0.01, 
-        2: 0.01, 
-        3: 0.01, 
-        4: 0.05, 
-        5: 0.15, 
-        6: 0.01, 
-        7: 0.01, 
-        8: 0.01, 
-        9: 0.05, 
-        10: 0.15, 
-        11: 0.01, 
-        12: 0.01, 
-        13: 0.01, 
-        14: 0.05, 
-        15: 0.15, 
-        16: 0.01, 
-        17: 0.01, 
-        18: 0.05, 
-        19: 0.15},
-        "TSIZE": 2,
-        "GRAMMAR": 'grammars/adaptive_autolr_grammar_mutate_level.txt',
-        "EXPERIMENT_NAME": 'dumps/test_engine',
-        "RUN": 1,
-        "INCLUDE_GENOTYPE": True,
-        "SAVE_STEP": 1,
-        "VERBOSE": True,
-        "EPOCHS": 2,
-        "VALIDATION_SIZE": 10,
-        "FITNESS_SIZE": 49980,
-        "BATCH_SIZE": 5,
-        "MIN_TREE_DEPTH": 6,
-        "MAX_TREE_DEPTH": 17,
-        "FITNESS_FLOOR": 0,
-        "PREPOPULATE": False,
-        "PATIENCE": 0,
-    }
-    from evaluators.adaptive_optimizer_evaluator_f_race import train_model_tensorflow_cifar10
-    evaluation_function = Optimizer_Evaluator_Tensorflow(train_model=train_model_tensorflow_cifar10)
-    sge.evolutionary_algorithm(parameters=parameters, evaluation_function=evaluation_function)
-
-def test_short_run_mnist():
-    import sge.grammar as grammar
-    import sge
-    from main import Optimizer_Evaluator_Tensorflow
-    from utils import create_models
-    create_models.create_models()
-    parameters = {
-        "POPSIZE": 2,
-        "GENERATIONS": 2,
-        "ELITISM": 0,   
-        "SEED": 0,                
-        "PROB_CROSSOVER": 0.0,
-        "PROB_MUTATION": {
-        0: 0.0, 
-        1: 0.01, 
-        2: 0.01, 
-        3: 0.01, 
-        4: 0.05, 
-        5: 0.15, 
-        6: 0.01, 
-        7: 0.01, 
-        8: 0.01, 
-        9: 0.05, 
-        10: 0.15, 
-        11: 0.01, 
-        12: 0.01, 
-        13: 0.01, 
-        14: 0.05, 
-        15: 0.15, 
-        16: 0.01, 
-        17: 0.01, 
-        18: 0.05, 
-        19: 0.15},
-        "TSIZE": 2,
-        "GRAMMAR": 'grammars/adaptive_autolr_grammar_mutate_level.txt',
-        "EXPERIMENT_NAME": 'dumps/test_engine',
-        "RUN": 1,
-        "INCLUDE_GENOTYPE": True,
-        "SAVE_STEP": 1,
-        "VERBOSE": True,
-        "EPOCHS": 2,
-        "VALIDATION_SIZE": 10,
-        "FITNESS_SIZE": 59980,
-        "BATCH_SIZE": 5,
-        "MIN_TREE_DEPTH": 6,
-        "MAX_TREE_DEPTH": 17,
-        "FITNESS_FLOOR": 0,
-        "PREPOPULATE": False,
-        "PATIENCE": 0,
-    }
-    from evaluators.adaptive_optimizer_evaluator_f_race import train_model_tensorflow_mnist
-    evaluation_function = Optimizer_Evaluator_Tensorflow(train_model=train_model_tensorflow_mnist)
-    sge.evolutionary_algorithm(parameters=parameters, evaluation_function=evaluation_function)
--- test/test_long.py
+++ test/test_long.py
@@ -53,3 +53,112 @@ def test_short_run():
     evaluation_function = Optimizer_Evaluator()
     sge.evolutionary_algorithm(parameters=parameters, evaluation_function=evaluation_function)
     ut.delete_directory(parameters['EXPERIMENT_NAME'], "run_1")
+
+def test_short_run_cifar10():
+    import sge.grammar as grammar
+    import sge
+    from main import Optimizer_Evaluator_Tensorflow
+    from utils import create_models
+    create_models.create_models()
+    parameters = {
+        "POPSIZE": 2,
+        "GENERATIONS": 2,
+        "ELITISM": 0,   
+        "SEED": 0,                
+        "PROB_CROSSOVER": 0.0,
+        "PROB_MUTATION": {
+        0: 0.0, 
+        1: 0.01, 
+        2: 0.01, 
+        3: 0.01, 
+        4: 0.05, 
+        5: 0.15, 
+        6: 0.01, 
+        7: 0.01, 
+        8: 0.01, 
+        9: 0.05, 
+        10: 0.15, 
+        11: 0.01, 
+        12: 0.01, 
+        13: 0.01, 
+        14: 0.05, 
+        15: 0.15, 
+        16: 0.01, 
+        17: 0.01, 
+        18: 0.05, 
+        19: 0.15},
+        "TSIZE": 2,
+        "GRAMMAR": 'grammars/adaptive_autolr_grammar_mutate_level.txt',
+        "EXPERIMENT_NAME": 'dumps/test_engine',
+        "RUN": 1,
+        "INCLUDE_GENOTYPE": True,
+        "SAVE_STEP": 1,
+        "VERBOSE": True,
+        "EPOCHS": 2,
+        "VALIDATION_SIZE": 10,
+        "FITNESS_SIZE": 49980,
+        "BATCH_SIZE": 5,
+        "MIN_TREE_DEPTH": 6,
+        "MAX_TREE_DEPTH": 17,
+        "FITNESS_FLOOR": 0,
+        "PREPOPULATE": False,
+        "PATIENCE": 0,
+    }
+    from evaluators.adaptive_optimizer_evaluator_f_race import train_model_tensorflow_cifar10
+    evaluation_function = Optimizer_Evaluator_Tensorflow(train_model=train_model_tensorflow_cifar10)
+    sge.evolutionary_algorithm(parameters=parameters, evaluation_function=evaluation_function)
+
+def test_short_run_mnist():
+    import sge.grammar as grammar
+    import sge
+    from main import Optimizer_Evaluator_Tensorflow
+    from utils import create_models
+    create_models.create_models()
+    parameters = {
+        "POPSIZE": 2,
+        "GENERATIONS": 2,
+        "ELITISM": 0,   
+        "SEED": 0,                
+        "PROB_CROSSOVER": 0.0,
+        "PROB_MUTATION": {
+        0: 0.0, 
+        1: 0.01, 
+        2: 0.01, 
+        3: 0.01, 
+        4: 0.05, 
+        5: 0.15, 
+        6: 0.01, 
+        7: 0.01, 
+        8: 0.01, 
+        9: 0.05, 
+        10: 0.15, 
+        11: 0.01, 
+        12: 0.01, 
+        13: 0.01, 
+        14: 0.05, 
+        15: 0.15, 
+        16: 0.01, 
+        17: 0.01, 
+        18: 0.05, 
+        19: 0.15},
+        "TSIZE": 2,
+        "GRAMMAR": 'grammars/adaptive_autolr_grammar_mutate_level.txt',
+        "EXPERIMENT_NAME": 'dumps/test_engine',
+        "RUN": 1,
+        "INCLUDE_GENOTYPE": True,
+        "SAVE_STEP": 1,
+        "VERBOSE": True,
+        "EPOCHS": 2,
+        "VALIDATION_SIZE": 10,
+        "FITNESS_SIZE": 59980,
+        "BATCH_SIZE": 5,
+        "MIN_TREE_DEPTH": 6,
+        "MAX_TREE_DEPTH": 17,
+        "FITNESS_FLOOR": 0,
+        "PREPOPULATE": False,
+        "PATIENCE": 0,
+    }
+    from evaluators.adaptive_optimizer_evaluator_f_race import train_model_tensorflow_mnist
+    evaluation_function = Optimizer_Evaluator_Tensorflow(train_model=train_model_tensorflow_mnist)
+    sge.evolutionary_algorithm(parameters=parameters, evaluation_function=evaluation_function)
+
