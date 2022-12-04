fcf00763fd3db4266429a7757ca0fe5885b5c82a
soren5 <pedrofilipecarvalho@mac.com>
2022-12-04 12:26:07 +0000

    Add LOAD ARCHIVE parameter

--- sge/engine.py
+++ sge/engine.py
@@ -100,6 +100,13 @@ def evolutionary_algorithm(evaluation_function=None, resume_generation=-1, param
     if 'RESUME' in params:
         logger.load_random_state(params['RESUME'])
         population = logger.load_population(params['RESUME'])
+        if 'LOAD_ARCHIVE' in params and params['LOAD_ARCHIVE'] == True:     
+            archive = logger.load_archive(params['RESUME'])
+            counter = int(np.max([archive[x]['id'] for x in archive]))
+        else:
+            archive = {}
+            id = len(population)
+            counter = id - 1
         archive = logger.load_archive(params['RESUME'])
         it = params['RESUME']
         counter = int(np.max([archive[x]['id'] for x in archive]))
