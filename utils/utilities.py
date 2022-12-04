3fe9b0f9ed0a7a55696e11929d43f90fb54decc4
soren5 <pedrofilipecarvalho@mac.com>
2022-12-04 13:35:34 +0000

    Fix delete directory utility

--- utils/utilities.py
+++ utils/utilities.py
@@ -1,12 +1,15 @@
 import os
 
-def delete_directory(experiment_name, run):
-    path = os.path.join(experiment_name, run)
-    for file_name in os.listdir(path):
-        # construct full file path
-        file = os.path.join(path, file_name)
-        if os.path.isfile(file):
-            print('Deleting file:', file)
-            os.remove(file)
-    os.rmdir(path)
+def delete_directory(experiment_name, runs):
+    if type(runs) != list:
+        runs = [runs]
+    for run in runs:
+        path = os.path.join(experiment_name, run)
+        for file_name in os.listdir(path):
+            # construct full file path
+            file = os.path.join(path, file_name)
+            if os.path.isfile(file):
+                print('Deleting file:', file)
+                os.remove(file)
+        os.rmdir(path)
     os.rmdir(experiment_name)
