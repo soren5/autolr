fac77de9e9c5377ba23941d3721308383a782f3f
swom <cicciacco@gmail.com>
2022-12-15 15:35:19 +0100

    re-organizing cluster scripts in folders, adding parameters to run large torunaments with old mutations

---
+++ cluster_scripts/run_sims_gpu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=63:00:00
+#SBATCH --job-name=autorl_gpu
+#SBATCH --output=autorl_gpu%j.log
+export PATH=$HOME/.local/bin:$PATH
+module load Python
+module load CUDA
+module load cuDNN
+pip install --upgrade pip
+pip install -r requirements.txt
+python -m utils.create_models
+python3 -m main --parameters parameters/adaptive_autolr_mutation_level.yml
