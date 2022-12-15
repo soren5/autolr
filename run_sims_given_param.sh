c8c2ce30f4b84091acecd3e298391e45a854d6e5
swom <cicciacco@gmail.com>
2022-12-15 15:38:17 +0100

    re-organizing cluster scripts in folders, adding parameters to run large torunaments with old mutations

--- run_sims_given_param.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
-#SBATCH --time=48:00:00
-#SBATCH --job-name=autorl_gpu
-#SBATCH --output=autorl_gpu%j.log
-python3 -m main --parameters ${1}
-
