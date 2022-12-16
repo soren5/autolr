400e5a7c2a8e43dc486c217ac3f7ad65c491bde4
swom <cicciacco@gmail.com>
2022-12-16 12:06:52 +0100

    refactoring the evolutionary engine

--- sge/engine.py
+++ sge/engine.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2 import driver
 from math import isclose
 from operator import inv
 import random
@@ -103,18 +104,191 @@ def setup(parameters=None, logger=None):
 
 def evolutionary_algorithm(evaluation_function=None, resume_generation=-1, parameters=None, logger_module=None):
     import os
+    logger = read_params(parameters, logger_module)
+    
+    check_google_colab()
+    
+    population, archive, counter, it = initialize_pop(logger)
+
+    return run_evolution(evaluation_function, os, logger, population, archive, counter, it)
+
+def run_evolution(evaluation_function, os, logger, population, archive, counter, it):
+    start_time = time.time()
+    while it <= params['GENERATIONS'] and (True if 'TIME_STOP' not in params else (True if time.time() - start_time < params['TIME_STOP'] else False)):
+        print(f"{it}")
+        for indiv in population:
+            update_archive(evaluation_function, archive, indiv)
+ 
+        updat_best_fitness(population, archive)
+       
+        for indiv in population:
+            update_key_and_fitness_based_on_archive(archive, indiv)
+        #print(f"{[x['id'] for x in population]}")
+
+        sort_pop_and_print_best_fit(population, it)
+        
+        save_data(logger, population, it)
+
+        if it == params['GENERATIONS']:
+            return population
+        reproduction_and_elitism(os, logger, population, archive, counter, it)
+
+def reproduction_and_elitism(os, logger, population, archive, counter, it):
+    new_population = reproduce_via_elitism(population)
+    reproduction(os, logger, population, archive, counter, it, new_population)
+
+def save_data(logger, population, it):
+    logger.evolution_progress(it, population)
+    logger.elicit_progress(it, population)
+
+def sort_pop_and_print_best_fit(population, it):
+    sort_pop_based_on_fitness(population)
+    print("\ngeneration: " + str(it) + "; best fit so far: " + str(population[0]['fitness']) + "\n")
+
+def read_params(parameters, logger_module):
     if logger_module != None:
         logger = logger_module
     else:
         import sge.logger as logger
     setup(parameters, logger_module)
-    
+    return logger
+
+def check_google_colab():
     if "COLAB" in params and params["COLAB"]:
         from google.colab import drive
         drive.mount('/content/drive')
-    
+
+def reproduction(os, logger, population, archive, counter, it, new_population):
+    while len(new_population) < params['POPSIZE']:
+        if random.random() < params['PROB_CROSSOVER']:
+            p1 = tournament(population, params['TSIZE'])
+            p2 = tournament(population, params['TSIZE'])
+            new_indiv = crossover(p1, p2)
+        else:
+            new_indiv = tournament(population, params['TSIZE'])
+        if type(params['PROB_MUTATION']) == float:
+            new_indiv = mutate(new_indiv, params['PROB_MUTATION'])
+        elif type(params['PROB_MUTATION']) == dict:
+            assert len(params['PROB_MUTATION']) == len(new_indiv['genotype'])
+            new_indiv = mutate_level(new_indiv, params['PROB_MUTATION'])
+        else:
+            raise Exception("Invalid mutation type")
+        mapping_values = [0 for i in new_indiv['genotype']]
+        phen, tree_depth = grammar.mapping(new_indiv['genotype'], mapping_values)
+        new_indiv['phenotype'] = phen
+        new_indiv['smart_phenotype'] = smart_phenotype(phen)
+        if new_indiv['smart_phenotype'] in archive:
+            new_indiv['id'] = archive[new_indiv['smart_phenotype']]['id']
+        else:
+            counter += 1
+            new_indiv['id'] = counter
+        new_population.append(new_indiv)
+    population = new_population
+    it += 1
+    logger.save_archive(it, archive)
+    logger.save_population(it, population)
+    logger.save_random_state(it)
+    if "COLAB" in params and params["COLAB"]:
+        driver.flush_and_unmount()
+        driver.Driver.mount('/content/drive')
+        import os
+        print(os.listdir(f"{params['EXPERIMENT_NAME']}/run_{params['RUN']}"))
+
+def reproduce_via_elitism(population):
+    new_population = population[:params['ELITISM']]
+    for indiv in new_population:
+        indiv['operation'] = 'elitism'
+    return new_population
+
+def sort_pop_based_on_fitness(population):
+    population.sort(key=lambda x: x['fitness'])
+
+def update_key_and_fitness_based_on_archive(archive, indiv):
+    key = update_key(indiv)
+    update_fitness_based_on_archive(archive, indiv, key)
+
+def update_fitness_based_on_archive(archive, indiv, key):
+    indiv['fitness'] = archive[key]['fitness']
+
+def update_key(indiv):
+    key = indiv['smart_phenotype']
+    return key
+
+def updat_best_fitness(population, archive):
+    best_fit = params['FITNESS_FLOOR'] + 1
+    for indiv in population:
+        key = indiv['smart_phenotype']
+        if archive[key]['fitness'] < best_fit:
+                # best = archive[key]
+            best_fit = archive[key]['fitness'] 
+        """    
+            to_remove = []
+            for eval_index in evaluation_indices:
+                eval_count = 0
+                # Iterate all the individuals, if they are statiscally different from the best, remove them from the next iteration of the cycle.
+                # If they are similar to best, re-evaluate
+                indiv = population[eval_index]
+                key = indiv['smart_phenotype']
+
+                if indiv['id'] != best['id']:
+                    try:
+                        stat, p_value = stats.mannwhitneyu(best['evaluations'], archive[indiv['smart_phenotype']]['evaluations'])
+                    except ValueError as e:
+                        p_value = 1
+                    if p_value < 0.05:
+                        to_remove.append(eval_index)
+                    else:
+                        # There is no statistical difference, re-evaluate
+                        key = indiv['smart_phenotype']  
+                        os.system("clear")    
+                        print(f"[{it}]-Reval: indiv {eval_count}/{len(evaluation_indices)} eval #{len(archive[key]['evaluations']) + 1}  {key}")
+                        evaluate(indiv, evaluation_function)
+                        archive[key]['evaluations'].append(indiv['fitness'])
+                        archive[key]['fitness'] = statistics.mean(archive[key]['evaluations']) 
+            for remove_index in to_remove:
+                evaluation_indices.remove(remove_index)
+            #ids_left = [population[x]["id"] for x in evaluation_indices]
+            if len(evaluation_indices) > 1:
+                try:
+                    stat, p_value_kruskal = stats.kruskal(*[archive[population[x]['smart_phenotype']]['evaluations'] for x in evaluation_indices])
+                except ValueError as e:
+                    p_value_kruskal = 1
+        """
+
+def update_archive(evaluation_function, archive, indiv):
+    indiv['smart_phenotype'] = smart_phenotype(indiv['phenotype'])
+    key = indiv['smart_phenotype']
+    if key in archive and 'fitness' not in archive[key]:
+        raise Exception('Incomplete archive entry')
+    if key not in archive:
+        archive[key] = {'evaluations': []}
+        archive[key]['id'] = indiv['id']
+                # evaluate seems to be deterministic. 
+                # Btw., if not, the caching of key|fitness pairs wouldn't be 100% correct
+        evaluate(indiv, evaluation_function)
+        archive[key]['evaluations'].append(indiv['fitness'])
+        archive[key]['fitness'] = statistics.mean(archive[key]['evaluations'])
+    """
+    # if in doubt (you should;), test:
+    for _ in range(5):                     
+        evaluate(indiv, evaluation_function)
+        archive[key]['evaluations'].append(indiv['fitness'])
+        archive[key]['fitness'] = statistics.mean(archive[key]['evaluations'])
+    deterministic = archive[key]['evaluations'][0]
+    for x in archive[key]['evaluations']:
+        if not isclose(x, deterministic):
+            raise "wrong assumption!"
+                
+    # `works` without:
+    try:
+        stat, p_value_kruskal = stats.kruskal(*[archive[population[x]['smart_phenotype']]['evaluations'] for x in evaluation_indices])
+    except ValueError as e:
+        p_value_kruskal = 1
+    while p_value_kruskal < 0.05 and len(evaluation_indices) > 1:
+    """
+
+def initialize_pop(logger):
     if 'RESUME' in params and params["RESUME"] != False:
-        
         if type(params["RESUME"]) == int: 
             last_gen = params['RESUME']
         elif params["RESUME"] == "Last":
@@ -150,132 +324,5 @@ def evolutionary_algorithm(evaluation_function=None, resume_generation=-1, param
                 phen, tree_depth = grammar.mapping(indiv['genotype'], mapping_values)
                 indiv['phenotype'] = phen
                 indiv['mapping_values'] = mapping_values
-
-    start_time = time.time()
-    while it <= params['GENERATIONS'] and (True if 'TIME_STOP' not in params else (True if time.time() - start_time < params['TIME_STOP'] else False)):
-        print(f"{it}")
-        evaluation_indices = list(range(len(population)))
-        indiv_count = 0
-        for indiv in population:
-            indiv['smart_phenotype'] = smart_phenotype(indiv['phenotype'])
-            key = indiv['smart_phenotype']
-            if key in archive and 'fitness' not in archive[key]:
-                raise Exception('Incomplete archive entry')
-            if key not in archive:
-                archive[key] = {'evaluations': []}
-                archive[key]['id'] = indiv['id']
-                # evaluate seems to be deterministic. 
-                # Btw., if not, the caching of key|fitness pairs wouldn't be 100% correct
-                evaluate(indiv, evaluation_function)
-                archive[key]['evaluations'].append(indiv['fitness'])
-                archive[key]['fitness'] = statistics.mean(archive[key]['evaluations'])
-        """
-                # if in doubt (you should;), test:
-                for _ in range(5):                     
-                    evaluate(indiv, evaluation_function)
-                    archive[key]['evaluations'].append(indiv['fitness'])
-                    archive[key]['fitness'] = statistics.mean(archive[key]['evaluations'])
-                deterministic = archive[key]['evaluations'][0]
-                for x in archive[key]['evaluations']:
-                    if not isclose(x, deterministic):
-                        raise "wrong assumption!"
-                
-
-        # `works` without:
-        try:
-            stat, p_value_kruskal = stats.kruskal(*[archive[population[x]['smart_phenotype']]['evaluations'] for x in evaluation_indices])
-        except ValueError as e:
-            p_value_kruskal = 1
-            
-        while p_value_kruskal < 0.05 and len(evaluation_indices) > 1:
-        """ 
-        best_fit = params['FITNESS_FLOOR'] + 1
-        for indiv in population:
-            key = indiv['smart_phenotype']
-            if archive[key]['fitness'] < best_fit:
-                # best = archive[key]
-                best_fit = archive[key]['fitness']
-        """    
-            to_remove = []
-            for eval_index in evaluation_indices:
-                eval_count = 0
-                # Iterate all the individuals, if they are statiscally different from the best, remove them from the next iteration of the cycle.
-                # If they are similar to best, re-evaluate
-                indiv = population[eval_index]
-                key = indiv['smart_phenotype']
-
-                if indiv['id'] != best['id']:
-                    try:
-                        stat, p_value = stats.mannwhitneyu(best['evaluations'], archive[indiv['smart_phenotype']]['evaluations'])
-                    except ValueError as e:
-                        p_value = 1
-                    if p_value < 0.05:
-                        to_remove.append(eval_index)
-                    else:
-                        # There is no statistical difference, re-evaluate
-                        key = indiv['smart_phenotype']  
-                        os.system("clear")    
-                        print(f"[{it}]-Reval: indiv {eval_count}/{len(evaluation_indices)} eval #{len(archive[key]['evaluations']) + 1}  {key}")
-                        evaluate(indiv, evaluation_function)
-                        archive[key]['evaluations'].append(indiv['fitness'])
-                        archive[key]['fitness'] = statistics.mean(archive[key]['evaluations']) 
-            for remove_index in to_remove:
-                evaluation_indices.remove(remove_index)
-            #ids_left = [population[x]["id"] for x in evaluation_indices]
-            if len(evaluation_indices) > 1:
-                try:
-                    stat, p_value_kruskal = stats.kruskal(*[archive[population[x]['smart_phenotype']]['evaluations'] for x in evaluation_indices])
-                except ValueError as e:
-                    p_value_kruskal = 1
-        """
-
-        for indiv in population:
-            key = indiv['smart_phenotype']
-            indiv['fitness'] = archive[key]['fitness']
-        #print(f"{[x['id'] for x in population]}")
-
-        population.sort(key=lambda x: x['fitness'])
-        logger.evolution_progress(it, population)
-        logger.elicit_progress(it, population)
-
-        print("\ngeneration: " + str(it) + "; best fit so far: " + str(population[0]['fitness']) + "\n")
-        if it == params['GENERATIONS']:
-            return population
-        new_population = population[:params['ELITISM']]
-        for indiv in new_population:
-            indiv['operation'] = 'elitism'
-        while len(new_population) < params['POPSIZE']:
-            if random.random() < params['PROB_CROSSOVER']:
-                p1 = tournament(population, params['TSIZE'])
-                p2 = tournament(population, params['TSIZE'])
-                new_indiv = crossover(p1, p2)
-            else:
-                new_indiv = tournament(population, params['TSIZE'])
-            if type(params['PROB_MUTATION']) == float:
-                new_indiv = mutate(new_indiv, params['PROB_MUTATION'])
-            elif type(params['PROB_MUTATION']) == dict:
-                assert len(params['PROB_MUTATION']) == len(new_indiv['genotype'])
-                new_indiv = mutate_level(new_indiv, params['PROB_MUTATION'])
-            else:
-                raise Exception("Invalid mutation type")
-            mapping_values = [0 for i in new_indiv['genotype']]
-            phen, tree_depth = grammar.mapping(new_indiv['genotype'], mapping_values)
-            new_indiv['phenotype'] = phen
-            new_indiv['smart_phenotype'] = smart_phenotype(phen)
-            if new_indiv['smart_phenotype'] in archive:
-                new_indiv['id'] = archive[new_indiv['smart_phenotype']]['id']
-            else:
-                counter += 1
-                new_indiv['id'] = counter
-            new_population.append(new_indiv)
-        population = new_population
-        it += 1
-        logger.save_archive(it, archive)
-        logger.save_population(it, population)
-        logger.save_random_state(it)
-        if "COLAB" in params and params["COLAB"]:
-            drive.flush_and_unmount()
-            drive.mount('/content/drive')
-            import os
-            print(os.listdir(f"{params['EXPERIMENT_NAME']}/run_{params['RUN']}"))
+    return population,archive,counter,it
 
