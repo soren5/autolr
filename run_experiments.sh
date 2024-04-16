a6e554d24b0d73bcc1775f9ff28ed2f8bded8b40
soren5 <pedrofilipecarvalho@mac.com>
2024-04-16 14:32:18 +0100

    Commit to merge with development

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,6 +3,6 @@ seeds=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 2
 run=1
 echo $seeds
 for s in ${seeds[*]}; do
-    CUDA_VISIBLE_DEVICES=1 python3 -m main --seed $s --parameters=parameters/journal_parameters/SM.yml --run $run
+    python -m main --seed $s --parameters=parameters/fake_fitness.yml --run $run
     run=$((run + 1))
 done
