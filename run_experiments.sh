22a45c06745c0475b7951e2f494e5e44e0e54ad9
soren5 <pedrofilipecarvalho@mac.com>
2024-07-09 12:46:58 +0100

    Run experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-seeds=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30)
+seeds=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
 run=1
 echo $seeds
 for s in ${seeds[*]}; do
-    CUDA_VISIBLE_DEVICES=1 python3 -m main --seed $s --parameters=parameters/journal_parameters/SM.yml --run $run
+    CUDA_VISIBLE_DEVICES=1 python3 -m a2 > out.log
     run=$((run + 1))
 done
