679ecde8747c67d9ca90b1de47cf842d390ae21e
Pedro <pedrofilipecarvalho@mac.com>
2026-02-17 16:44:41 +0000

    Update dataset settings experiment

--- _experiments/dataset_settings.py
+++ _experiments/dataset_settings.py
@@ -10,37 +10,43 @@ from tensorflow.keras.optimizers import Adam
 
 
 configurations = [
-    {'task': 'FMNIST', "all_false": True},
-    {'task': 'FMNIST', "all_false": False},
+    #{'task': 'FMNIST', "all_false": True},
+    #{'task': 'FMNIST', "all_false": False},
+
+    #{'task': 'CIFAR10', "all_false": True},
+    #{'task': 'CIFAR10', "all_false": False},
 
     {'task': 'Tiny-Imagenet', "all_false": True},
     {'task': 'Tiny-Imagenet', "all_false": False},
-
-    {'task': 'CIFAR10', "all_false": True},
-    {'task': 'CIFAR10', "all_false": False},
-
 ]
-num_runs = 30
+num_repetitions = 10
 
 
-def main():
+def main(i):
     with open("parameters/multi_task.yml", 'r') as ymlfile:
         parameters = yaml.load(ymlfile, Loader=yaml.FullLoader)
         reset_parameters()
         manual_load_parameters(parameters)
         params['FAKE_FITNESS'] = False
-        for config in configurations:
-            if config['task'] == 'Tiny-Imagenet':
-                evaluator = TINY_IMAGENET_Evaluator(params, task_name='tiny_imagenet')
-            elif config['task'] == 'CIFAR10':
-                evaluator = CIFAR10_Evaluator(params, task_name='cifar10')
-            elif config['task'] == 'FMNIST':
-                evaluator = FMNIST_Evaluator(params, task_name='fmnist')
-            for run in range(num_runs):
-                optimizer = Adam()
-                optimizer.name = f'Adam + {config["task"]} + {config["all_false"]}'
+        for run in range(i, i + num_repetitions):
+            for config in configurations:
+                if config['task'] == 'Tiny-Imagenet':
+                    evaluator = TINY_IMAGENET_Evaluator(params, task_name=f"{config['task'].lower()}_{config['all_false']}")
+                elif config['task'] == 'CIFAR10':
+                    evaluator = CIFAR10_Evaluator(params, task_name=f"{config['task'].lower()}_{config['all_false']}")
+                elif config['task'] == 'FMNIST':
+                    evaluator = FMNIST_Evaluator(params, task_name=f"{config['task'].lower()}_{config['all_false']}")
+                    optimizer = Adam()
+                    optimizer.name = f'Adam + {config["task"]} + {config["all_false"]}'
 
-                evaluator.evaluate_optimizer(optimizer)
+                    evaluator.run = run
+                    evaluator.evaluate_optimizer(optimizer)
 
 if __name__ == '__main__':
-    main()
+    # Get the first argument passed on to the script and use it as the starting number for runs
+    import sys
+    if len(sys.argv) > 1:
+        i = int(sys.argv[1])
+    else:        
+        i = 0
+    main(i) 
