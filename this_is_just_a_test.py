8edbadf47b88f63e75532b317935dd0283a924b8
Pedro Carvalho <pedrofilipecarvalho@mac.com>
2026-02-24 16:51:11 +0000

    Tentative fix to cluster permission problems

--- this_is_just_a_test.py
+++ this_is_just_a_test.py
@@ -1,10 +1,12 @@
+import os
+# This ONE LINE fixes all subsequent file/directory operations
+os.umask(0o002)
 # try to create a file in the root of the project 
 with open('test_file.txt', 'w') as f:
     f.write('This is just a test file to check if the file watcher is working correctly.\n')
 
 # try to create a file in drive/bai/pfcarvalho/logs, create missing directories if necessary
-import os
-log_dir = 'drive/bai/pfcarvalho/logs'
+log_dir = 'drive/pfcarvalho/logs'
 os.makedirs(log_dir, exist_ok=True)
 with open(os.path.join(log_dir, 'test_log.txt'), 'w') as f:
     f.write('This is just a test log file to check if the file watcher is working correctly.\n')
