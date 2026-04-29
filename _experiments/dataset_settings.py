934e12955af18dd0a1b0c93ad33a2c6f09f4ca43
Pedro Carvalho <pedrofilipecarvalho@mac.com>
2026-04-29 15:25:03 +0100

    Add new dataset_settings

--- _experiments/dataset_settings.py
+++ _experiments/dataset_settings.py
@@ -5,6 +5,7 @@ import yaml
 from evaluators.evaluate_fmnist import FMNIST_Evaluator
 from evaluators.evaluate_cifar10 import CIFAR10_Evaluator
 from evaluators.evaluate_tiny_imagenet import TINY_IMAGENET_Evaluator
+from evaluators.evaluate_cifar100 import CIFAR100_Evaluator
 from sge.parameters import reset_parameters, manual_load_parameters, params
 from tensorflow.keras.optimizers import Adam
 
@@ -15,7 +16,10 @@ configurations = [
     #{'task': 'CIFAR10', "pre_process": True},
     #{'task': 'CIFAR10', "pre_process": False},
 
-    {'task': 'Tiny-Imagenet', "pre_process": True},
+    {'task': 'CIFAR100', 'pre_process': True},
+    #{'task': 'CIFAR100', 'pre_process': False},
+
+    #{'task': 'Tiny-Imagenet', "pre_process": True},
     #{'task': 'Tiny-Imagenet', "pre_process": False},
 ]
 num_repetitions = 15
@@ -27,12 +31,13 @@ def main(i):
             for config in configurations:
                 with open("parameters/multi_task.yml", 'r') as ymlfile:
                     parameters = yaml.load(ymlfile, Loader=yaml.FullLoader)
-                    parameters['EXPERIMENT_NAME'] = "adam_optimizer_tin_test_using_dataset_settings"
+                    parameters['EXPERIMENT_NAME'] = "adam_optimizer_cifar100_test_using_dataset_settings"
                     reset_parameters()
                     manual_load_parameters(parameters)
                 if config['pre_process']:
                     params['FMNIST_CONFIG'] = 'parameters/dataset_parameters/FMNIST_CONFIG.json'
                     params['CIFAR10_CONFIG'] = 'parameters/dataset_parameters/CIFAR10_CONFIG.json'
+                    params['CIFAR10_CONFIG'] = 'parameters/dataset_parameters/CIFAR100_CONFIG.json'
                     params['TINY_IMAGENET_CONFIG'] = 'parameters/dataset_parameters/TINY_IMAGENET_CONFIG.json'
                 else:
                     params['FMNIST_CONFIG'] = 'parameters/dataset_parameters/FMNIST_CONFIG_all_false.json'
@@ -42,6 +47,8 @@ def main(i):
                 print(params)
                 if config['task'] == 'Tiny-Imagenet':
                     evaluator = TINY_IMAGENET_Evaluator(params, task_name=f"{config['task'].lower()}_{config['pre_process']}")
+                elif config['task'] == 'CIFAR1000':
+                    evaluator = CIFAR100_Evaluator(params, task_name=f"{config['task'].lower()}_{config['pre_process']}")
                 elif config['task'] == 'CIFAR10':
                     evaluator = CIFAR10_Evaluator(params, task_name=f"{config['task'].lower()}_{config['pre_process']}")
                 elif config['task'] == 'FMNIST':
