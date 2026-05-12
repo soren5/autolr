cc62ec78096ce5539bf35e742718c1aa0b24889e
Pedro Carvalho <pedrofilipecarvalho@mac.com>
2026-05-12 18:11:30 +0100

    Add verbosity to create_models

--- utils/create_models.py
+++ utils/create_models.py
@@ -14,6 +14,7 @@ def create_models():
             model = keras.models.model_from_json(model_json)
             path = os.path.join(os.getcwd(), "models", filename.name[:-5] + ".h5")
             model.save(path)
+            print(f"Saved model to {path}")
             
 if __name__ == "__main__":
     create_models()
